16f3e1798d0beeab492274dd9c4557a6a93a7ff0
Lymio <Lymio@users.noreply.github.com>
2025-01-22 23:05:22 +0100

    railway fix

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,7 @@ setupUnhandledErrors();
 const app = express();
 
 const corsOptions = {
-    origin: 'http://localhost:5173', 
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173', 
     methods: ['GET', 'POST'],
     credentials: true,
   };
